a886259f59dc3d598ff22d8ec9a473e9e1b27dac
kyle1james <kyle1james@gmail.com>
2023-03-22 09:33:31 -0400

    robot init script

--- install.py
+++ install.py
@@ -2,6 +2,7 @@ import subprocess
 # update and upgrade os
 # Enable the universe repository
 subprocess.call(['sudo', 'add-apt-repository', 'universe'])
+subprocess.call(['sudo', 'apt-get', '-y', 'rpi-update'])
 # Update the package list
 subprocess.call(['sudo', 'apt-get', 'update'])
 # Install pip and packages needed for cv2
@@ -11,9 +12,10 @@ subprocess.call(['sudo', 'apt-get', 'upgrade', '-y'])
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'make', 'libssl-dev', 'libghc-zlib-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev', 'gettext'])
 # AI packages
 subprocess.call(['sudo', 'pip', 'install', 'numpy', 'pandas', 'scipy'])
-# Install OpenCV
+# Install OpenCV with apt-get & headless
 subprocess.call(['sudo', 'apt-get', 'install', '-y', ' python3-opencv'])
 # Install scikit-learn
 subprocess.call(['sudo', 'pip', 'install', 'scikit-learn'])
 # Install Visual Studio Code
 #subprocess.call(['sudo', 'apt-get', 'install', '-y', 'code'])
+subprocess.call(['sudo','reboot'])
