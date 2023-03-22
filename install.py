92502dc28a59500c5e20fc00420b95a6994e328b
kyle1james <kyle1james@gmail.com>
2023-03-22 09:23:19 -0400

    robot init script

--- install.py
+++ install.py
@@ -2,23 +2,18 @@ import subprocess
 # update and upgrade os
 # Enable the universe repository
 subprocess.call(['sudo', 'add-apt-repository', 'universe'])
-
 # Update the package list
 subprocess.call(['sudo', 'apt-get', 'update'])
-
-# Install pip
+# Install pip and packages needed for cv2
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python3-pip'])
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'build-essential', 'cmake', 'pkg-config', 'libjpeg-dev', 'libtiff5-dev', 'libpng-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libv4l-dev', 'libxvidcore-dev', 'libx264-dev', 'libgtk-3-dev', 'libatlas-base-dev', 'gfortran', 'python3-dev'])
 subprocess.call(['sudo', 'apt-get', 'upgrade', '-y'])
-
-
-# Install the packages
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'make', 'libssl-dev', 'libghc-zlib-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev', 'gettext'])
 # AI packages
 subprocess.call(['sudo', 'pip', 'install', 'numpy', 'pandas', 'scipy'])
 # Install OpenCV
-subprocess.call(['sudo', 'pip', 'install', '-y', 'python-opencv'])
+subprocess.call(['sudo', 'apt-get', 'install', '-y', ' python3-opencv'])
 # Install scikit-learn
 subprocess.call(['sudo', 'pip', 'install', 'scikit-learn'])
 # Install Visual Studio Code
-subprocess.call(['sudo', 'apt-get', 'install', '-y', 'code'])
+#subprocess.call(['sudo', 'apt-get', 'install', '-y', 'code'])
