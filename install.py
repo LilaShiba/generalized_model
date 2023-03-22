d12c8bd54eeaa1601870788cca3b4e43edb2ebff
kyle1james <kyle1james@gmail.com>
2023-03-22 09:15:07 -0400

    robot setup script

--- install.py
+++ install.py
@@ -8,7 +8,9 @@ subprocess.call(['sudo', 'apt-get', 'update'])
 
 # Install pip
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python3-pip'])
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'build-essential', 'cmake', 'pkg-config', 'libjpeg-dev', 'libtiff5-dev', 'libpng-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libv4l-dev', 'libxvidcore-dev', 'libx264-dev', 'libgtk-3-dev', 'libatlas-base-dev', 'gfortran', 'python3-dev'])
 subprocess.call(['sudo', 'apt-get', 'upgrade', '-y'])
+
 
 # Install the packages
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'make', 'libssl-dev', 'libghc-zlib-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev', 'gettext'])
