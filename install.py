9d620354ee64a38fbb9b29de5d4669c99a8a2726
kyle1james <kyle1james@gmail.com>
2023-03-22 09:12:16 -0400

    robot setup script

--- install.py
+++ install.py
@@ -7,15 +7,13 @@ subprocess.call(['sudo', 'add-apt-repository', 'universe'])
 subprocess.call(['sudo', 'apt-get', 'update'])
 
 # Install pip
-subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-pip'])
-subprocess.call(['sudo', 'apt-get', 'update', '-y'])
-subprocess.call(['sudo', 'apt', 'install', '-y', 'python-pip'])
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python3-pip'])
 subprocess.call(['sudo', 'apt-get', 'upgrade', '-y'])
 
 # Install the packages
 subprocess.call(['sudo', 'apt-get', 'install', '-y', 'make', 'libssl-dev', 'libghc-zlib-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev', 'gettext'])
 # AI packages
-subprocess.call(['sudo', 'pip', 'install', '-y', 'numpy', 'pandas', 'scipy'])
+subprocess.call(['sudo', 'pip', 'install', 'numpy', 'pandas', 'scipy'])
 # Install OpenCV
 subprocess.call(['sudo', 'pip', 'install', '-y', 'python-opencv'])
 # Install scikit-learn
