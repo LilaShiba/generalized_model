4e381956a7391b5dc6a20f7fa2235a165a4b7f62
kyle1james <kyle1james@gmail.com>
2023-03-22 08:42:34 -0400

    robot setup script

--- install.py
+++ install.py
@@ -1,7 +1,14 @@
 import subprocess
 
-subprocess.check_call(['pip', 'install', 'numpy'])
-subprocess.check_call(['pip', 'install', 'pandas'])
-subprocess.check_call(['pip', 'install', 'scipy'])
-subprocess.check_call(['pip', 'install', 'opencv-python'])
-subprocess.check_call(['pip', 'install', 'sklearn.datasets'])
+# Install NumPy
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-numpy'])
+# Install Pandas
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-pandas'])
+# Install SciPy
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-scipy'])
+# Install OpenCV
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-opencv'])
+# Datasets from scipy
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'sklearn.datasets'])
+# Install Visual Studio Code
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'code'])
