47a20a92ade0c37dd40b56122f828a450e0c9e0f
kyle1james <kyle1james@gmail.com>
2023-03-22 09:06:17 -0400

    robot setup script

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import subprocess
 # update and upgrade os
 subprocess.call(['sudo', 'apt-get', 'update', '-y'])
+subprocess.call(['sudo', 'apt-get', 'install', '-y', 'python-pip'])
 subprocess.call(['sudo', 'apt-get', 'upgrade', '-y'])
 
 # Install the packages
