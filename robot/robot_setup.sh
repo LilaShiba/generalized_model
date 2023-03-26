d3fc89a644113cc4c74323f7a96d08df8a2878b5
kyle1james <kyle1james@gmail.com>
2023-03-26 14:31:15 -0400

    robot

--- robot/robot_setup.sh
+++ robot/robot_setup.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-# Purge the installed packages
-sudo apt-get purge -y python3 python3-pip libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test motion
-
-# Remove any remaining configuration files
-sudo rm -rf /etc/motion
-
-# Remove any remaining Python libraries
-sudo pip3 uninstall -y opencv-python-headless numpy pandas scipy pillow
-
-# Update the package list and upgrade the installed packages
-sudo apt-get update && sudo apt-get upgrade -y
-
-# Install Python and pip
-sudo apt-get install python3 python3-pip -y
-
+sudo apt-get-update
+sudo apt full-upgrade
+sudo apt-get install python3-picamera eog git vlc -y
 # Install OpenCV dependencies
 sudo apt-get install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test -y
-
-# Install OpenCV
-sudo pip3 install opencv-python-headless
-
-# Install numpy, pandas, and scipy
-sudo pip3 install numpy pandas scipy
-
-# Install motion
-sudo apt-get install motion -y
-
+# Remove any remaining Python libraries
+sudo pip install -y opencv-python-headless numpy pandas scipy pillow
 # Install PIL
-sudo pip3 install pillow
+sudo pip install pillow
 
-echo "Purge and reinstallation complete!"
+echo "Purge and Reinstallation complete!"
