14d8df436310a6b9bfe9b0737462dfc4136bc451
kyle1james <kyle1james@gmail.com>
2023-03-26 15:23:20 -0400

    meow

--- robot/robot_setup.sh
+++ robot/robot_setup.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get-update
 sudo apt full-upgrade
-sudo apt-get install python3-picamera eog git vlc -y
+# Base Packages
+sudo apt-get install python3-picamera libatlas-base-dev eog git vlc code realvnc-vnc-server realvnc-vnc-viewer -y
 # Install OpenCV dependencies
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test -y
-# Remove any remaining Python libraries
-sudo pip install -y opencv-python-headless numpy pandas scipy pillow
-# Install PIL
-sudo pip install pillow
-
+sudo apt-get install python3-dev python3-pip python3-numpy libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libgtkglext1-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103
+# Python Basics
+sudo pip install -y numpy pandas scipy pillow matplotlib
+# Install CV2
+sudo pip install -y opencv-python-headless
 echo "Purge and Reinstallation complete!"
